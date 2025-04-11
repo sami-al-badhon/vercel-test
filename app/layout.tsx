0e3fdbadc73844f07c317b0d6ede7a52adff23b9
sami-al-badhon <mdsamialbadhon@gmail.com>
2025-04-11 21:33:39 +0600

    'test2'

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { WebVitals } from './_components/web-vitals';
+
 import { GoogleAnalytics } from '@next/third-parties/google';
 
 const geistSans = Geist({
@@ -28,7 +28,6 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <WebVitals />
         {children}
 
         <GoogleAnalytics gaId="G-STVPBNWG1F" />
